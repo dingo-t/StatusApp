3980ace481fe80512c0fe1108876551e7ff8462d
dingo-t <101374201+dingo-t@users.noreply.github.com>
2024-09-30 06:52:21 +1300

    Iteration 1.6

--- components/MessageList.js
+++ components/MessageList.js
@@ -1,18 +1,22 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import MessageItem from './MessageItem'
+import { View, Text, ScrollView } from 'react-native';
+import React from 'react';
+import MessageItem from './MessageItem';
 
 // this component maps out each object in the messages variable and sends it to the messageItem component to be rendered
-export default function MessageList({messages, scrollViewRef,currentUser}) {
+export default function MessageList({ messages, scrollViewRef, currentUser }) {
+  let index = 0;
+  let messageItems = [];
+
+  while (index < messages.length) {
+    messageItems.push(
+      <MessageItem message={messages[index]} key={index} currentUser={currentUser} />
+    );
+    index++;
+  }
+
   return (
-    <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false} contentContainerStyle={{paddingTop: 10}}>
-      {
-        messages.map((message, index)=>{
-            return (
-              <MessageItem message={message} key={index} currentUser={currentUser} />
-            )
-        })
-      }
+    <ScrollView ref={scrollViewRef} showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingTop: 10 }}>
+      {messageItems}
     </ScrollView>
-  )
+  );
 }
